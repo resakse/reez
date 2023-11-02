b67e49839081109a57482f9a5511cec251fa5c11
resakse <resakse@gmail.com>
2023-11-02 13:00:22 +0800

    settings.py

--- reez/settings.py
+++ reez/settings.py
@@ -150,10 +150,10 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
